53143ca4d42c1eb52f96281389a1ed6ddefbb607
AntonioDavid333 <adarte82899@gmail.com>
2026-01-17 14:19:07 +0100

    controller-updated

--- scanner-redes/scanner-redes/src/main/java/scanner_redes/controllers/TelemetriaController.java
+++ scanner-redes/scanner-redes/src/main/java/scanner_redes/controllers/TelemetriaController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import scanner_redes.dto.ComprobacionResponseDTO;
 import scanner_redes.repositories.ComprobacionRepository;
+import scanner_redes.repositories.RedRepository;
 
 
 import java.sql.Timestamp;
@@ -30,6 +31,8 @@ public class TelemetriaController {
     List<ComprobacionResponseDTO> comprobaciones=new ArrayList<>();
     @Autowired
     ComprobacionRepository comprobacionRepository;
+    @Autowired
+    RedRepository redRepository;
 
     /**
      *
@@ -41,11 +44,14 @@ public class TelemetriaController {
         if (comprobacion == null) {
             return ResponseEntity.badRequest().body("No fue posible guardar la comprobación de red.");
         }
+        if (!redRepository.existsBySsid(comprobacion.getSsid())){
+            return ResponseEntity.badRequest().body("Esta red no está guardada en el sistema.");
+        }
         ComprobacionResponseDTO compAdd = new ComprobacionResponseDTO(comprobacion.getSsid(), comprobacion.getEstado(), new Date());
         Comprobacion compSave = new Comprobacion(comprobacion.getSsid(), comprobacion.getEstado(), new Timestamp(new Date().getTime()));
-        if (comprobacionRepository.existsBySsid(compSave.getSsid())) {
-            return ResponseEntity.badRequest().body("Esta comprobación ya está guardada");
-        }
+//        if (comprobacionRepository.existsBySsid(compSave.getSsid())) {
+//            return ResponseEntity.badRequest().body("Esta comprobación ya está guardada");
+//        }
         comprobacionRepository.save(compSave);
         return ResponseEntity.ok().body(compSave);
     }
